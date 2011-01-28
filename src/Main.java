e5b73025887ea76f20035bbcb0832a42b95e5b5b
Latoof <Latoof@014-Serv-Updt.mshome.net>
2011-01-29 00:13:16 +0100

    Retablissement (test)
    Signed-off-by: Latoof <Latoof@014-Serv-Updt.mshome.net>

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !");
+		System.out.println("J'aime Nils!");
+		System.out.println("lol");
+	}
+
+}
