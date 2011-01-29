5a33cbfe5eb5ff86a96b9de8bd78286e380d8acd
Latoof <Latoof@014-Serv-Updt.mshome.net>
2011-01-29 13:27:36 +0100

    Ajout d'une proposition dans Plateau
    Signed-off-by: Latoof <Latoof@014-Serv-Updt.mshome.net>

--- src/Plateau.java
+++ src/Plateau.java
@@ -6,6 +6,14 @@
  * @author E086089A
  *
  */
+////////
+// Proposition pour le "tableau" de Bille.
+// Une MAP avec les Cases en index, et les Billes en valeur.
+// En gros : map<Case,Bille> tableauBilles.
+// 
+// Cela permettrait d'optimiser la recherche d'une Bille (variable) dans une case (fixe).
+// Je vous laisse y réfléchir.
+///////
 public class Plateau {
 
 }
