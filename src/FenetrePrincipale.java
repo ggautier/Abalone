64ab10582ec124b3cc9bc6b94f0c0b1c37e2e6c6
Anaguiros <matthieu.lenogue@gmail.com>
2011-02-01 20:37:31 +0100

    Ajout UML "Final", et début partie graphique (Fenetre et menuBarre).
    Signed-off-by: Anaguiros <matthieu.lenogue@gmail.com>

--- src/FenetrePrincipale.java
+++ src/FenetrePrincipale.java
@@ -18,6 +18,67 @@
  * 
  * @version 1.0
  */
-public class FenetrePrincipale {
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class FenetrePrincipale extends JFrame{
+
+	private JPanel 		panel;
+	private JMenuBar 	menuBar;
+	private JMenu		fichierMenu;
+
+	public FenetrePrincipale(String titre)
+	{
+		//Héritage du builder de la super classe JFrame
+		super(titre);
+		
+		//Variable propre à la fenetre ("fermable" et redimensionable)
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(true);
+        
+        //On cree un barre de menu (vide), puis on cree le 1er menu "Fichier", dans lequel on ajoute l'item "Nouveau", ....
+        JMenuBar menuBar = new JMenuBar();
+        this.setJMenuBar(menuBar);
+        
+        JMenu fichierMenu = new JMenu("Fichier");
+        
+        JMenuItem item = new JMenuItem("Nouveau");
+        fichierMenu.add(item);
+        item = new JMenuItem("Sauvegarder");
+        fichierMenu.add(item);
+        item = new JMenuItem("Charger");
+        fichierMenu.add(item);
+        item = new JMenuItem("Options");
+        fichierMenu.add(item);
+        item = new JMenuItem("Quitter");
+        fichierMenu.add(item);
+        menuBar.add(fichierMenu);
+        
+        
+        panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+	}
+	
+	void donnerContrainte(GridBagConstraints gbc, int gx, int gy, int gw, int gh, int
+			wx, int wy)
+			    {
+			            gbc.gridx=gx;
+			            gbc.gridy=gy;
+			            gbc.gridwidth=gw;
+			            gbc.gridheight=gh;
+			            gbc.weightx=wx;
+			            gbc.weighty=wy;
+			            gbc.fill=GridBagConstraints.BOTH;
+			    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.Dimension;
 
 public class Main {
 
@@ -6,9 +7,9 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World !");
-		System.out.println("J'aime Nils!");
-		System.out.println("lol");
+		FenetrePrincipale f = new FenetrePrincipale("Abalone - 1.00");
+        f.setSize(new Dimension(950,725));
+        f.setVisible(true);
 	}
 
 }
