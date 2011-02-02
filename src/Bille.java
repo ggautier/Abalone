b9436d36440c1d88310b1b53d593e8e6c7af66ed
ggautier <gaetan.gautier@etu.univ-angers.fr>
2011-02-02 18:13:41 +0100

    Ajout des accesseur et mutateur de Bille

--- src/Bille.java
+++ src/Bille.java
@@ -1,15 +1,15 @@
+// TODO Fixer le cas des billes mortes
+//		-> supprimer l'objet ?
+//		-> l'indiquer dans un booléen ?
+//		-> quid de la case ?
+
 /**
- * <b>Bille est la classe qui représente une bille durant une partie.</b>
+ * <b>Bille est la classe qui represente une bille durant une partie.</b>
  * <p>
- * Un Bille est caractérisée par les informations suivantes :
- * <ul>
- * <li>Un joueur, qui va posséder la bille.</li>
- * <li>Une case, que la bille va occuper à un instant.</li>
- * </ul>
+ * Une Bille est caracterisee par un joueur qui va la posseder.
  * </p>
  * 
  * @see Joueur
- * @see Case
  * 
  * @author Lenogue Matthieu
  * @author Gautier Quentin
@@ -19,14 +19,27 @@
  * @version 1.0
  */
 public class Bille {
-
-/*
- * Maxime le 29.01.11 a 0:13
- * Retablissement effectue
- * Test de conflit ligne 6 : B
- * Simple suppression des lignes d'avertissement, puis commit individuel du fichier, puis push du projet.
- * Test de conflit ligne 6 : Z
- * Bonne nuit
- */
 	
+	/**
+	 * Le joueur proprietaire objet.
+	 * 
+	 * @see Joueur
+	 */
+	private Joueur joueur;
+	
+	public Bille(Joueur newJoueur)
+	{
+		this.joueur = newJoueur;
+	}
+	
+	/**
+	 * Accesseur du joueur
+	 * 
+	 * @return Le joueur proprietaire de la bille.
+	 * 
+	 * @see Joueur
+	 */
+	public Joueur getJoueur() {
+		return this.joueur;
+	}
 }
