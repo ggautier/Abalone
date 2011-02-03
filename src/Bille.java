f5c93739fa0a5ec650570435ac0b93a6035d0d1b
Latoof <Latoof@192.168.2.111>
2011-02-03 16:15:12 +0100

    Modif de "Bille" (coords et joueur)
    Signed-off-by: Latoof <Latoof@192.168.2.111>

--- src/Bille.java
+++ src/Bille.java
@@ -19,14 +19,46 @@
  * @version 1.0
  */
 public class Bille {
+	protected int coordX;
+	protected int coordY;
+	protected Joueur joueur;
+	
+	Bille(int i, int j, Joueur player) {
+		this.coordX = i;
+		this.coordY = j;
+		this.joueur = player;
+		
+	}
 
-/*
- * Maxime le 29.01.11 a 0:13
- * Retablissement effectue
- * Test de conflit ligne 6 : B
- * Simple suppression des lignes d'avertissement, puis commit individuel du fichier, puis push du projet.
- * Test de conflit ligne 6 : Z
- * Bonne nuit
- */
+	public int getX() {
+		return coordX;
+	}
+
+	public void setX(int coordX) {
+		this.coordX = coordX;
+	}
+
+	public int getY() {
+		return coordY;
+	}
+
+	public void setY(int coordY) {
+		this.coordY = coordY;
+	}
+
+	public Joueur getJoueur() {
+		return joueur;
+	}
+
+	public void setJoueur(Joueur joueur) {
+		this.joueur = joueur;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 }
