b16de902f8c593377698d91cdb6f618e34e78677
Anaguiros <matthieu.lenogue@gmail.com>
2011-02-15 16:58:15 +0100

    sauvegarde pour gaetan.
    Signed-off-by: Anaguiros <matthieu.lenogue@gmail.com>

--- src/vue/FenetrePrincipale.java
+++ src/vue/FenetrePrincipale.java
@@ -205,15 +205,8 @@ public class FenetrePrincipale extends JFrame implements ActionListener{
 						BufferedWriter out = new BufferedWriter(lu);
 						// Mettre le flux en tampon (en cache)
 						
-						// affichage du plateau (toString())
-						out.write(this.getControleur().getPartie().getPlateau().toString()); 
-						out.write("\n");
-						//affichage du joueur actif
-						int i = (this.getControleur().getPartie().getJCourant().getCamps() ? 1 : 0) ;
-						String temp = new String(""+i);
-						out.write(temp);
-						out.write("\n");
 						//affichage du joueur 1 (Nom r g b score humain)
+						String temp = new String();
 						temp = this.getControleur().getPartie().getJ1().getNom() + " ";
 						temp += this.getControleur().getPartie().getJ1().getR() + " ";
 						temp += this.getControleur().getPartie().getJ1().getG() + " ";
@@ -223,6 +216,7 @@ public class FenetrePrincipale extends JFrame implements ActionListener{
 
 						out.write(temp);
 						out.write("\n");
+						
 						
 						//affichage du joueur 2 (Nom r g b score humain)
 						temp = this.getControleur().getPartie().getJ2().getNom() + " ";
@@ -234,6 +228,18 @@ public class FenetrePrincipale extends JFrame implements ActionListener{
 
 						out.write(temp);
 						out.write("\n");
+						
+						// affichage du plateau (toString())
+						out.write(this.getControleur().getPartie().getPlateau().toString()); 
+						out.write("\n");
+						//affichage du joueur actif
+						int i = (this.getControleur().getPartie().getJCourant().getCamps() ? 1 : 0) ;
+						temp = ""+i;
+						out.write(temp);
+						out.write("\n");
+						
+						
+						
 
 						// Balancer dans le flux le contenu de la zone de texte
 						out.close(); 
