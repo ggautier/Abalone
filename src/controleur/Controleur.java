89a6d1d27428447772e0a1030996a031f3cdeda7
Latoof <Latoof@192.168.4.219>
2011-02-15 12:52:15 +0100

    Ajout de variables pour la Vue, et legere optimisation : Notre jeu est maintenant beautiful ! (pas de redimensionnement, mais attendra)
    Signed-off-by: Latoof <Latoof@192.168.4.219>

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -5,6 +5,7 @@ import java.awt.Point;
 import java.awt.PointerInfo;
 import java.util.Vector;
 
+import vue.FenetrePlateau;
 import vue.FenetrePrincipale;
 
 import modele.Bille;
@@ -286,13 +287,28 @@ public class Controleur {
 	
 	// Retourne une Bille a partir d'un point de coordonnes (utilitaire)
 	public Point getBillePointee(Point p) {
+		FenetrePlateau fen = this.fenetrePrincipale.getPlateau();
 
+		Point pRetour = new Point();
+		double iBille = (p.getY()-this.fenetrePrincipale.getPlateau().getLongueur()/2)
+				/fen.getLongueur();
+		
+		int i = (int) Math.round(iBille);
+		//int decalage = (int) (4-iBille)*23;
+		double jBille = ((p.getX()-fen.getLongueur()/2)-(4-i)*
+				(int) (fen.getWidth()*0.042)-fen.getOffsetL())
+				/fen.getLargeur();
+		int j = (int) Math.round(jBille);
+		
+		/*
+		pRetour.setLocation(i,j);
 		Point pRetour = new Point();
 		double iBille = (p.getY()-20)/40.0;
 		int i = (int) Math.round(iBille);
 		//int decalage = (int) (4-iBille)*23;
 		double jBille = ((p.getX()-20)-(4-i)*23)/45.0;
 		int j = (int) Math.round(jBille);
+		*/
 		
 		pRetour.setLocation(i,j);
 
