6f7d8cc8f9f647d62c4b3f62074020703885d6bd
Anaguiros <matthieu.lenogue@gmail.com>
2011-02-16 14:57:45 +0100

    fin de soirée.
    Signed-off-by: Anaguiros <matthieu.lenogue@gmail.com>

--- src/vue/FenetreCommande.java
+++ src/vue/FenetreCommande.java
@@ -123,7 +123,7 @@ public class FenetreCommande extends JPanel implements ActionListener {
 		if(source == "Unselect")
 		{	
 			//méthode de desélection propre au controleur
-			//this.fenetre.getControleur().deselectionner();
+			this.fenetre.getControleur().deselectionner();
 		}
 	}
 }
