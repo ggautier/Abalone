3b3f53588a085b05ef1fa90047da45ea90828a96
Latoof <Latoof@activate.adobe.com127.0.0.1>
2011-02-20 20:50:14 +0100

    KIIIIIIWIIIIIII (mec bourré dans le train) Les sauvegardes/chargement fonctionnent à nouveau. Reste à nettoyer le code.
    Signed-off-by: Latoof <Latoof@activate.adobe.com127.0.0.1>

--- src/vue/FenetrePrincipale.java
+++ src/vue/FenetrePrincipale.java
@@ -207,33 +207,7 @@ public class FenetrePrincipale extends JFrame implements ActionListener{
 
 						out.write(temp);
 
-						out.write("\n");
-						
-						
-						//affichage du joueur 2 (Nom r g b score humain)
-						temp = this.getControleur().getPartie().getJ2().getNom() + " ";
-						temp += this.getControleur().getPartie().getJ2().getR() + " ";
-						temp += this.getControleur().getPartie().getJ2().getG() + " ";
-						temp += this.getControleur().getPartie().getJ2().getB() + " ";
-						temp += this.getControleur().getPartie().getJ2().getScore() + " ";
-						temp += this.getControleur().getPartie().getJ2().isHumain() + " ";
-
-						out.write(temp);
-						out.write("\n\n");
-						
-						// affichage du plateau (toString())
-						out.write(this.getControleur().getPartie().getPlateau().toString()); 
-						out.write("\n");
-						//affichage du joueur actif
-						int i = (this.getControleur().getPartie().getJCourant().getCamps() ? 1 : 0) ;
-						temp = ""+i;
-						out.write(temp);
-						out.write("\n");
-						
-						
-						
-
-						// Balancer dans le flux le contenu de la zone de texte
+									// Balancer dans le flux le contenu de la zone de texte
 						out.close(); 
 						// Fermer le flux (c'est toujours mieux de le fermer explicitement)
 					} 
