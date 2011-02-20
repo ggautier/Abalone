90eb23f5e06c82e27c9005132f0040204550f98d
Latoof <Latoof@activate.adobe.com127.0.0.1>
2011-02-20 20:38:20 +0100

    Fenetre de fin de Partie fonctionnelle. (reste à pouvoir acceder au plateau par defaut par une variable definissable dans les options, pas par un texte brut)
    Signed-off-by: Latoof <Latoof@activate.adobe.com127.0.0.1>

--- src/vue/FenetreOver.java
+++ src/vue/FenetreOver.java
@@ -3,6 +3,7 @@ package vue;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -11,8 +12,11 @@ import javax.swing.border.TitledBorder;
 
 public class FenetreOver extends JDialog implements ActionListener{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private FenetrePrincipale		fenetre;
-	private JDialog					dialog;
 	private JPanel 					panel;
 	private JLabel					text;
 	private JButton					playAgain, quit;
@@ -22,12 +26,14 @@ public class FenetreOver extends JDialog implements ActionListener{
 	
 		this.fenetre = fenetre;
 
-		dialog = new JDialog();
-		dialog.setSize(300, 200);
-		dialog.setTitle(titre);
-		dialog.setVisible(true);
-		dialog.setAlwaysOnTop(true);
-		dialog.setContentPane(buildContentPane());
+		this.setSize(300, 200);
+		this.setTitle(titre);
+	    this.setLocationRelativeTo(fenetre);
+	    this.setLocation((int)fenetre.getLocation().getX()+200,(int)fenetre.getLocation().getY()+100);
+	    
+	    this.setVisible(true);
+	    this.setAlwaysOnTop(true);
+	    this.setContentPane(buildContentPane());
 	
 	}
 	
@@ -37,8 +43,10 @@ public class FenetreOver extends JDialog implements ActionListener{
 		this.panel = new JPanel();
         this.panel.setLayout(new GridBagLayout());
 	    GridBagConstraints c = new GridBagConstraints();
+
 		
-        text = new JLabel(this.fenetre.getControleur().getPartie().getJCourant().getNom());
+        //text = new JLabel(this.fenetre.getControleur().getPartie().getJCourant().getNom());
+        text = new JLabel("Voulez-vous rejouer ?");
         
         donnerContrainte(c,0,0,2,1,0,0);
         panel.add(text,c);
@@ -49,10 +57,13 @@ public class FenetreOver extends JDialog implements ActionListener{
 		quit.setText("Quitter");
         
         donnerContrainte(c,0,1,1,1,0,0);
-        panel.add(text,c);
+        panel.add(playAgain,c);
         donnerContrainte(c,1,1,1,1,0,0);
-        panel.add(text,c);
-
+        panel.add(quit,c);
+        
+        playAgain.addActionListener(this);
+        quit.addActionListener(this);
+        
         
 		return panel;
 		
@@ -73,7 +84,19 @@ public class FenetreOver extends JDialog implements ActionListener{
 	// le bouton OK a son propre listener qui prend la fenetre en param
 	public void actionPerformed(ActionEvent e) {
 		
-		Object source = e.getActionCommand();
+		Object source = e.getSource();
+		
+		if (source.equals(this.playAgain))
+			try {
+				this.fenetre.getControleur().getPartie().chargerParFichier("./data/plateau/defaut.plt");
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		else if (source.equals(this.quit)) 
+			this.fenetre.dispose(); // Un truc pour quitter ici.
+		
+		this.fenetre.rafraichir();
+		this.dispose();
 
 		
 	}
