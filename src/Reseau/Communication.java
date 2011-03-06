129499d570a45f1b0910d7d7f1773f9e1d222e93
Latoof <Latoof@192.168.4.120>
2011-03-06 17:02:14 +0100

    Non-teste (non-commente) --> Ebauche pour l'etablissement de la premiere connexion (recuperer l'iP des partenaires), puis transit des coups
    Signed-off-by: Latoof <Latoof@192.168.4.120>

--- src/Reseau/Communication.java
+++ src/Reseau/Communication.java
@@ -5,14 +5,165 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class Communication {
+import controleur.Controleur;
 
-	public Communication() {
+public class Communication {
+	
+	protected boolean heberge;
+	
+	protected String ip_partenaire;
+	
+	protected int port;
+	
+	protected Controleur controleur;
+	
+	protected boolean contact_etabli;
+
+	
+	public Communication(Controleur cont, String ip, int port) throws IOException {
 		
+		this.controleur = cont;
+		
+		if (ip.equals("") || ip == null)
+			this.heberge = true;
+		else {
+			this.heberge = false;
+			this.ip_partenaire = ip;
+		}
+		
+		this.contact_etabli = false;
+		this.port = port;
+		
+		this.etablir_contact();
+		
+		
+	}
+	
+	public boolean etablir_contact() throws IOException {
+		
+		if (heberge) {
+			ServerSocket server = new ServerSocket(this.port);
+			Socket client = server.accept();
+			
+			BufferedWriter writerS = new BufferedWriter(new OutputStreamWriter(
+					client.getOutputStream()));
+			BufferedReader readerS = new BufferedReader(new InputStreamReader(
+					client.getInputStream()));
+			
+			String line;
+			String str = "";
+			while ( (line = readerS.readLine()) != null) {
+				System.out.println("Recu Serveur : "+line);
+				if (line.equals("C_CONTACT")) {
+					writerS.write("OK\n");
+					writerS.flush();
+					this.contact_etabli = true;
+				} else if (this.contact_etabli)
+					this.ip_partenaire = line;
+			}
+			
+			
+			readerS.close();
+			writerS.close();
+			client.close();
+			server.close();
+		}
+		else {
+			Socket s = new Socket(this.ip_partenaire,this.port);
+			
+			s.getOutputStream().write(("C_CONTACT\n"+InetAddress.getLocalHost().getHostAddress()+"\n").getBytes());
+			
+			BufferedWriter writerC = new BufferedWriter(new OutputStreamWriter(
+					s.getOutputStream()));
+			BufferedReader readerC = new BufferedReader(new InputStreamReader(
+					s.getInputStream()));
+			
+			String g = "TEST";
+		
+			writerC.write(g);
+			writerC.flush();
+			
+			String ligne;
+			while((ligne = readerC.readLine()) != null) {
+						
+				System.out.println("Recu Client : " + ligne);
+				if (ligne.equals("OK"))
+					this.contact_etabli = true;
+			
+			}
+			
+			writerC.close();
+			readerC.close();
+			s.close();
+			
+			return true;
+		}
+		
+		return true;
+		
+	}
+	
+	public String attendre_coup() throws IOException {
+		
+		ServerSocket server = new ServerSocket(this.port);
+		Socket client = server.accept();
+		
+		BufferedWriter writerS = new BufferedWriter(new OutputStreamWriter(
+				client.getOutputStream()));
+		BufferedReader readerS = new BufferedReader(new InputStreamReader(
+				client.getInputStream()));
+		
+		String line;
+		String str = "";
+		while ( (line = readerS.readLine()) != null) {
+			System.out.println("Recu Serveur : "+line);
+			if (line.equals("Coup")) {
+				writerS.write("OK !\n");
+				writerS.flush();
+			}
+		}
+		
+		
+		readerS.close();
+		writerS.close();
+		client.close();
+		server.close();
+		
+		return str;
+
+	}
+		
+	public boolean envoyer_coup() throws UnknownHostException, IOException {
+		Socket s = new Socket(this.ip_partenaire,this.port);
+		
+		s.getOutputStream().write(("Test803\n").getBytes());
+		
+		BufferedWriter writerC = new BufferedWriter(new OutputStreamWriter(
+				s.getOutputStream()));
+		BufferedReader readerC = new BufferedReader(new InputStreamReader(
+				s.getInputStream()));
+		
+		String g = "TEST";
+	
+		writerC.write(g);
+		writerC.flush();
+		
+		String ligne;
+		while((ligne = readerC.readLine()) != null) {
+					
+			System.out.println("Recu Client : " + ligne);
+		}
+		
+		writerC.close();
+		readerC.close();
+		s.close();
+		
+		return true;
 	}
 
 	public static void testClient() throws UnknownHostException, IOException {
